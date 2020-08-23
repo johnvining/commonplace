15aade6a79e89193b1aeddb3f4f09524d2fb4b75
John Vining <johnvining@gmail.com>
2020-08-23 09:47:20 -0500

    Fix jumping from one idea to another

--- site/Idea.js
+++ site/Idea.js
@@ -3,7 +3,7 @@ import Note from './Note'
 import * as db from './Database'
 
 class Idea extends React.Component {
-  state = { loading: true }
+  state = { loading: true, newId: false }
 
   // TODO: Clean up -- https://stackoverflow.com/questions/48139281/react-doesnt-reload-component-data-on-route-param-change-or-query-change
   // TODO: DRY
@@ -18,7 +18,6 @@ class Idea extends React.Component {
       .catch(error => {
         console.error(error)
       })
-
     db.getIdeaInfo(this.props.id)
       .then(response => {
         this.setState({
@@ -28,6 +27,41 @@ class Idea extends React.Component {
       .catch(error => {
         console.error(error)
       })
+  }
+
+  componentDidUpdate() {
+    if (this.state.newId) {
+      this.setState({ newId: false }, () => {
+        db.getNotesForIdea(this.props.id)
+          .then(response => {
+            this.setState({
+              notes: response.data.data
+            })
+          })
+          .catch(error => {
+            console.error(error)
+          })
+        db.getIdeaInfo(this.props.id)
+          .then(response => {
+            this.setState({
+              ideaName: response.data.data.name
+            })
+          })
+          .catch(error => {
+            console.error(error)
+          })
+      })
+    }
+  }
+
+  static getDerivedStateFromProps(next, prev) {
+    if (next.id != prev.id) {
+      return {
+        id: next.id,
+        newId: true
+      }
+    }
+    return next
   }
 
   render() {
