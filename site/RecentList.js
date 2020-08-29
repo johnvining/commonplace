9bfdd2da9ebe03482e39346a3c7e2212af812c1e
John Vining <johnvining@gmail.com>
2020-08-29 16:58:33 -0500

    Remove old key handling in RecentList

--- site/RecentList.js
+++ site/RecentList.js
@@ -4,15 +4,7 @@ import NoteList from './NoteList'
 import * as db from './Database'
 
 class RecentList extends React.Component {
-  state = { loading: true }
-
-  componentDidMount() {
-    console.log('recentList-componentDidMount')
-    document.addEventListener('keydown', this.handleKeyDown.bind(this), false)
-  }
-
   async getListOfNotes() {
-    console.log('recentList-fetchListOfNotes')
     let notesResponse
     await db
       .getRecentNotes()
@@ -24,34 +16,6 @@ class RecentList extends React.Component {
       })
 
     return notesResponse
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener(
-      'keydown',
-      this.handleKeyDown.bind(this),
-      false
-    )
-  }
-
-  handleKeyDown(event) {
-    // TODO: Keyboard short cuts will interfere with Ctrl + A on Windows
-    if (event.keyCode == 13) {
-      // TODO: This is clumsy -- need to avoid inner elements like Input's or anything that happens while note is being edited
-      if (document.activeElement.className == 'noteNormal-outer') {
-        this.setIsFocused(document.activeElement.id)
-      }
-    }
-  }
-
-  setIsFocused(id) {
-    this.setState({
-      inFocus: id
-    })
-  }
-
-  becomeInFocus(id) {
-    this.setState({ inFocus: id })
   }
 
   render() {
