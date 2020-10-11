fbafdb5d0c995d48d9f6fc4ebd2677d68f97641a
John Vining <johnvining@gmail.com>
2020-10-10 21:53:58 -0500

    Fix deleting notes state error

--- site/Note.js
+++ site/Note.js
@@ -193,7 +193,7 @@ class Note extends React.Component {
 
   handleFocusImage(click) {
     if (
-      this.state.mode == constants.note_modes.EDIT &&
+      this.props.mode == constants.note_modes.EDIT &&
       confirm('Are you sure you want to delete this image?')
     ) {
       if (click.target.id == this.state.largeImage) {
