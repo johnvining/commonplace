0372c3014a3f56c62502c0a54fd3e8ce3b143ff7
John Vining <johnvining@gmail.com>
2020-10-17 21:17:29 -0500

    Account for res having statusCode already

--- server/src/utils/requests.js
+++ server/src/utils/requests.js
@@ -2,9 +2,7 @@ export const asyncWrapper = (requestHandler, successCode) => {
   return async function(req, res) {
     try {
       const data = await requestHandler(req, res)
-      if (!res.statusCode) {
-        res.status(successCode).json({ data: data })
-      }
+      res.status(successCode).json({ data: data })
     } catch (e) {
       console.error(e)
       res.status(400).end()
