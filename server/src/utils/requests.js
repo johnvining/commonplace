c118e41e385f8a978265991ae157aa3dfd5b846c
John Vining <johnvining@gmail.com>
2020-10-17 20:52:49 -0500

    Update requests.js

--- server/src/utils/requests.js
+++ server/src/utils/requests.js
@@ -1,5 +1,6 @@
 export const asyncWrapper = (requestHandler, successCode) => {
   return async function(req, res) {
+    console.log('wrapper')
     try {
       const data = await requestHandler(req, res)
       if (!res.statusCode) {
