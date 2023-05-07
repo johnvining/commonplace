5c07056acbe43ee4f8a105574d3527a8902d5ddb
John Vining <johnvining@gmail.com>
2023-05-07 13:23:41 -0500

    Update tokens

--- server/src/utils/suggestions.js
+++ server/src/utils/suggestions.js
@@ -31,7 +31,7 @@ export const getSuggestedIdeas = async function(note_title, note_text) {
       '\n' +
       note_text,
     temperature: 0.3,
-    max_tokens: 130
+    max_tokens: 150
   })
   const suggested_tags = completion.data.choices[0].text
     .replaceAll('\n', '')
