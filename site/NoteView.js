4ba10ff3c47e0a5121831b55582565491b0222b9
John Vining <johnvining@gmail.com>
2023-05-28 09:33:03 -0500

    Convert NoteView.js to function component, use useParams()

--- site/NoteView.js
+++ site/NoteView.js
@@ -2,51 +2,48 @@ import * as constants from './constants'
 import * as db from './Database'
 import NoteList from './NoteList'
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
-class NoteView extends React.Component {
-  state = {
-    id: ''
-  }
+function NoteView(props) {
+  const { id } = useParams()
 
-  async getListOfOneNote(index, page) {
+  const getListOfOneNote = async () => {
     var notesResponse
     var noteId
-    if (!this.props.id) {
+    if (!id) {
       const nick = await db.getNick(this.props.nick)
       noteId = nick.data.data.note
     } else {
-      noteId = this.props.id
+      noteId = id
     }
 
     await db
       .getInfo(db.types.note, noteId)
-      .then(response => {
+      .then((response) => {
         notesResponse = response
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error)
       })
     if (notesResponse.data.data[0].title) {
-      this.props.setPageTitle(notesResponse.data.data[0].title)
+      props.setPageTitle(notesResponse.data.data[0].title)
     } else if (notesResponse.data.data[0].text) {
-      this.props.setPageTitle(notesResponse.data.data[0].text)
+      props.setPageTitle(notesResponse.data.data[0].text)
     } else {
-      this.props.setPageTitle('Untitled Note')
+      props.setPageTitle('Untitled Note')
     }
 
     return notesResponse
   }
 
-  render() {
-    return (
-      <NoteList
-        key={'note' + (this.props.id ?? this.props.nick)}
-        viewMode={constants.view_modes.FULL}
-        editFirst={this.props.edit}
-        getListOfNotes={this.getListOfOneNote.bind(this)}
-      />
-    )
-  }
+  return (
+    <NoteList
+      key={'note' + (id ?? this.props.nick)}
+      viewMode={constants.view_modes.FULL}
+      editFirst={props.edit}
+      getListOfNotes={getListOfOneNote}
+    />
+  )
 }
 
 export default NoteView
