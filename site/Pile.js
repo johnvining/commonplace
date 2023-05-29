d4b726c2e6e31ed4b2eedc214296edb36f05bada
John Vining <johnvining@gmail.com>
2023-05-28 21:30:24 -0500

    Migrate pile.js and pilelistforitem.js to hooks

--- site/Pile.js
+++ site/Pile.js
@@ -1,4 +1,4 @@
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import * as constants from './constants'
 import * as db from './Database'
 import NoteList from './NoteList'
@@ -6,64 +6,45 @@ import React from 'react'
 import WorkList from './WorkList'
 import YearSpan from './YearSpan'
 import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
-class Pile extends React.Component {
-  state = {
-    id: '',
-    edit: false,
-    pendingName: '',
-    pendingStartYear: '',
-    pendingEndYear: '',
-  }
+function Pile(props) {
+  const { id } = useParams()
+  const [edit, setEdit] = useState(false)
+  const [pendingName, setPendingName] = useState('')
+  const [pendingStartYear, setPendingStartYear] = useState('')
+  const [pendingEndYear, setPendingEndYear] = useState('')
+  const navigate = useNavigate()
 
-  componentDidMount() {
-    this.fetchPileInfo(this.props.id)
-  }
-
-  componentDidUpdate(prevState) {
-    if (prevState.id !== this.state.id) {
-      this.fetchPileInfo(this.state.id)
-    }
-  }
-
-  fetchPileInfo(pileId) {
+  const fetchPileInfo = (pileId) => {
     db.getInfo(db.types.pile, pileId)
       .then((response) => {
-        this.setState({
-          pendingName: response.data.data.name,
-          pendingStartYear: response.data.data.start_year,
-          pendingEndYear: response.data.data.end_year,
-        })
+        setPendingName(response.data.data.name)
+        setPendingStartYear(response.data.data.start_year)
+        setPendingEndYear(response.data.data.end_year)
       })
       .catch((error) => {
         console.error(error)
       })
   }
 
-  static getDerivedStateFromProps(nextProps, prevState) {
-    if (nextProps.id !== prevState.id) {
-      return { id: nextProps.id }
-    }
+  useEffect(() => {
+    fetchPileInfo(id)
+  }, [id])
 
-    return null
-  }
-
-  async handleDeletePile() {
-    if (
-      !confirm(`Do you want to permanently delete '${this.state.pendingName}'?`)
-    ) {
+  const handleDeletePile = async () => {
+    if (!confirm(`Do you want to permanently delete '${pendingName}'?`)) {
       return
     }
 
-    await db.deleteRecord(db.types.pile, this.state.id)
-    const navigate = useNavigate()
+    await db.deleteRecord(db.types.pile, id)
     navigate('/')
   }
 
-  async getListOfNotes() {
+  const getListOfNotes = async () => {
     var notesResponse
     await db
-      .getRecordsWithFilter(db.types.note, db.types.pile, this.state.id)
+      .getRecordsWithFilter(db.types.note, db.types.pile, id)
       .then((response) => {
         notesResponse = response
       })
@@ -74,10 +55,10 @@ class Pile extends React.Component {
     return notesResponse
   }
 
-  async getListOfWorks() {
+  const getListOfWorks = async () => {
     var worksResponse
     await db
-      .getRecordsWithFilter(db.types.work, db.types.pile, this.state.id)
+      .getRecordsWithFilter(db.types.work, db.types.pile, id)
       .then((response) => {
         worksResponse = response
       })
@@ -88,136 +69,125 @@ class Pile extends React.Component {
     return worksResponse
   }
 
-  async handleAcceptUpdates() {
+  const handleAcceptUpdates = async () => {
     var updateObject = {
-      name: this.state.pendingName,
-      start_year: this.state.pendingStartYear,
-      end_year: this.state.pendingEndYear,
+      name: pendingName,
+      start_year: pendingStartYear,
+      end_year: pendingEndYear,
     }
 
-    db.updateRecord(db.types.pile, this.props.id, updateObject)
-    this.setState({ edit: false })
+    db.updateRecord(db.types.pile, id, updateObject)
+    setEdit(false)
   }
 
-  render() {
-    this.props.setPageTitle(this.state.pendingName)
-    return (
-      <div>
-        {/* Header and Edit */}
-        <div key="pile-information">
-          {this.state.edit ? (
-            <>
-              <label htmlFor="title" className="work-page form-label">
-                Name
-              </label>
-              <input
-                className="work-page title input"
-                id="title"
-                defaultValue={this.state.pendingName}
-                onChange={(e) => {
-                  this.setState({ pendingName: e.target.value })
-                }}
-              />
-              <label htmlFor="startYear" className="work-page form-label">
-                Start Year
-              </label>
-              <input
-                className="work-page title input"
-                id="startYear"
-                defaultValue={this.state.pendingStartYear}
-                onChange={(e) => {
-                  this.setState({ pendingStartYear: e.target.value })
-                }}
-              />
-              <label htmlFor="endYear" className="work-page form-label">
-                End Year
-              </label>
-              <input
-                className="work-page title input"
-                id="endYear"
-                defaultValue={this.state.pendingEndYear}
-                onChange={(e) => {
-                  this.setState({ pendingEndYear: e.target.value })
-                }}
-              />
-              <button
-                className="top-level standard-button left-right"
-                onClick={this.handleAcceptUpdates.bind(this)}
-              >
-                Done
-              </button>
-            </>
-          ) : (
-            <>
-              <div className="page-title">{this.state.pendingName}</div>
-              {this.state.pendingStartYear || this.state.pendingEndYear ? (
-                <div className="page-sub-title">
-                  {this.state.pendingStartYear ? (
-                    <YearSpan year={this.state.pendingStartYear} />
-                  ) : null}
-                  {this.state.pendingStartYear && this.state.pendingEndYear
-                    ? ' to '
-                    : null}
-                  {this.state.pendingEndYear ? (
-                    <YearSpan year={this.state.pendingEndYear} />
-                  ) : null}
-                </div>
-              ) : null}
-              <div>
-                <button
-                  className="top-level standard-button"
-                  onClick={this.handleDeletePile.bind(this)}
-                >
-                  Delete
-                </button>
-                <button
-                  className="top-level standard-button"
-                  onClick={() => {
-                    this.setState({ edit: true })
-                  }}
-                >
-                  Edit
-                </button>
-                {this.props.showNotes ? (
-                  ''
-                ) : (
-                  <Link to={'/pile/' + this.state.id + '/notes'}>
-                    <button
-                      className="top-level standard-button"
-                      style={{ marginLeft: '4px' }} // Margins come from standard-button + standard-button which doesn't work becasue of the <a>
-                    >
-                      Notes
-                    </button>
-                  </Link>
-                )}
-              </div>
-            </>
-          )}
-        </div>
-
-        {/* Note and Work List */}
-        {this.props.showNotes ? (
-          <NoteList
-            key={'noteList' + this.props.id}
-            viewMode={this.props.viewMode}
-            getListOfNotes={this.getListOfNotes.bind(this)}
-          />
+  props.setPageTitle(pendingName)
+  return (
+    <div>
+      {/* Header and Edit */}
+      <div key="pile-information">
+        {edit ? (
+          <>
+            <label htmlFor="title" className="work-page form-label">
+              Name
+            </label>
+            <input
+              className="work-page title input"
+              id="title"
+              defaultValue={pendingName}
+              onChange={(e) => {
+                setPendingName(e.target.value)
+              }}
+            />
+            <label htmlFor="startYear" className="work-page form-label">
+              Start Year
+            </label>
+            <input
+              className="work-page title input"
+              id="startYear"
+              defaultValue={pendingStartYear}
+              onChange={(e) => {
+                setPendingStartYear(e.target.value)
+              }}
+            />
+            <label htmlFor="endYear" className="work-page form-label">
+              End Year
+            </label>
+            <input
+              className="work-page title input"
+              id="endYear"
+              defaultValue={pendingEndYear}
+              onChange={(e) => {
+                setPendingEndYear(e.target.value)
+              }}
+            />
+            <button
+              className="top-level standard-button left-right"
+              onClick={handleAcceptUpdates}
+            >
+              Done
+            </button>
+          </>
         ) : (
           <>
-            <WorkList
-              key={'workList' + this.props.id}
-              getListOfWorks={this.getListOfWorks.bind(this)}
-            />
-            <NoteList
-              key={'noteList' + this.props.id}
-              viewMode={constants.view_modes.RESULT}
-              getListOfNotes={this.getListOfNotes.bind(this)}
-            />
+            <div className="page-title">{pendingName}</div>
+            {pendingStartYear || pendingEndYear ? (
+              <div className="page-sub-title">
+                {pendingStartYear ? <YearSpan year={pendingStartYear} /> : null}
+                {pendingStartYear && pendingEndYear ? ' to ' : null}
+                {pendingEndYear ? <YearSpan year={pendingEndYear} /> : null}
+              </div>
+            ) : null}
+            <div>
+              <button
+                className="top-level standard-button"
+                onClick={handleDeletePile}
+              >
+                Delete
+              </button>
+              <button
+                className="top-level standard-button"
+                onClick={() => {
+                  setEdit(true)
+                }}
+              >
+                Edit
+              </button>
+              {props.showNotes ? (
+                ''
+              ) : (
+                <Link to={'/pile/' + id + '/notes'}>
+                  <button
+                    className="top-level standard-button"
+                    style={{ marginLeft: '4px' }} // Margins come from standard-button + standard-button which doesn't work becasue of the <a>
+                  >
+                    Notes
+                  </button>
+                </Link>
+              )}
+            </div>
           </>
         )}
       </div>
-    )
-  }
+
+      {/* Note and Work List */}
+      {props.showNotes ? (
+        <NoteList
+          key={'noteList' + id}
+          viewMode={props.viewMode}
+          getListOfNotes={getListOfNotes}
+        />
+      ) : (
+        <>
+          <WorkList key={'workList' + id} getListOfWorks={getListOfWorks} />
+          <NoteList
+            key={'noteList' + id}
+            viewMode={constants.view_modes.RESULT}
+            getListOfNotes={getListOfNotes}
+          />
+        </>
+      )}
+    </div>
+  )
 }
 
 export default Pile
--- site/PileListForItem.js
+++ site/PileListForItem.js
@@ -3,46 +3,44 @@ import Autocomplete from './Autocomplete'
 import { useNavigate } from 'react-router-dom'
 import * as db from './Database'
 
-class PileListForItem extends React.Component {
-  render() {
-    return (
-      <>
-        {this.props.piles?.map((pile) => (
-          <button
-            key={'/pile/' + pile._id}
-            className={this.props.remove ? 'pile label remove' : 'pile label'}
-            onClick={() => {
-              if (this.props.remove) {
-                this.props.onPileRemove(pile._id)
-              } else {
-                const navigate = useNavigate()
-                navigate('/pile/' + pile._id)
-              }
-            }}
-          >
-            {pile.name}
-          </button>
-        ))}
-        {this.props.edit ? (
-          <Autocomplete
-            className={this.props.mainClassName + ' pile-select'}
-            clearOnSelect={true}
-            defaultValue=""
-            dontAutofocus={false}
-            escape={this.props.escape}
-            getSuggestions={this.props.getSuggestions}
-            apiType={db.types.pile}
-            handleNewSelect={this.props.handleNewSelect.bind(this)}
-            inputName="work-pile"
-            onSelect={this.props.onSelect.bind(this)}
-            excludeIds={this.props.piles?.map((pile) => pile._id)}
-          />
-        ) : (
-          ''
-        )}
-      </>
-    )
-  }
+function PileListForItem(props) {
+  const navigate = useNavigate()
+  return (
+    <>
+      {props.piles?.map((pile) => (
+        <button
+          key={'/pile/' + pile._id}
+          className={props.remove ? 'pile label remove' : 'pile label'}
+          onClick={() => {
+            if (props.remove) {
+              props.onPileRemove(pile._id)
+            } else {
+              navigate('/pile/' + pile._id)
+            }
+          }}
+        >
+          {pile.name}
+        </button>
+      ))}
+      {props.edit ? (
+        <Autocomplete
+          className={props.mainClassName + ' pile-select'}
+          clearOnSelect={true}
+          defaultValue=""
+          dontAutofocus={false}
+          escape={props.escape}
+          getSuggestions={props.getSuggestions}
+          apiType={db.types.pile}
+          handleNewSelect={props.handleNewSelect.bind(this)}
+          inputName="work-pile"
+          onSelect={props.onSelect.bind(this)}
+          excludeIds={props.piles?.map((pile) => pile._id)}
+        />
+      ) : (
+        ''
+      )}
+    </>
+  )
 }
 
 export default PileListForItem
