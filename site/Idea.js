b02db5fa45df8c932e7e7081e92ce896b428ec9d
John Vining <johnvining@gmail.com>
2023-06-12 19:59:51 -0500

    Convert Idea.js to function

--- site/Idea.js
+++ site/Idea.js
@@ -1,46 +1,37 @@
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import * as db from './Database'
 import NoteList from './NoteList'
 import React from 'react'
 import YearSpan from './YearSpan'
+import { useState, useEffect } from 'react'
 
-class Idea extends React.Component {
-  state = {
-    id: '',
-    edit: false,
-    pendingName: '',
-    pendingStartYear: '',
-    pendingEndYear: '',
-  }
+function Idea(props) {
+  const { id } = useParams()
+  const [edit, setEdit] = useState(false)
+  const [pendingName, setPendingName] = useState('')
+  const [pendingStartYear, setPendingStartYear] = useState('')
+  const [pendingEndYear, setPendingEndYear] = useState('')
 
-  componentDidMount() {
-    this.fetchIdeaInfo(this.props.id)
-  }
-
-  componentDidUpdate(prevState) {
-    if (prevState.id !== this.state.id) {
-      this.fetchIdeaInfo(this.state.id)
-    }
-  }
-
-  fetchIdeaInfo(ideaId) {
+  const fetchIdeaInfo = (ideaId) => {
     db.getInfo(db.types.idea, ideaId)
       .then((response) => {
-        this.setState({
-          pendingName: response.data.data.name,
-          pendingStartYear: response.data.data.start_year,
-          pendingEndYear: response.data.data.end_year,
-        })
+        setPendingName(response.data.data.name)
+        setPendingStartYear(response.data.data.start_year)
+        setPendingEndYear(response.data.data.end_year)
       })
       .catch((error) => {
         console.error(error)
       })
   }
 
-  async getListOfNotes() {
+  useEffect(() => {
+    fetchIdeaInfo(id)
+  }, [id])
+
+  const getListOfNotes = async () => {
     var notesResponse
     await db
-      .getRecordsWithFilter(db.types.note, db.types.idea, this.state.id)
+      .getRecordsWithFilter(db.types.note, db.types.idea, id)
       .then((response) => {
         notesResponse = response
       })
@@ -51,130 +42,113 @@ class Idea extends React.Component {
     return notesResponse
   }
 
-  static getDerivedStateFromProps(nextProps, prevState) {
-    if (nextProps.id !== prevState.id) {
-      return { id: nextProps.id }
-    }
-
-    return null
-  }
-
-  async deleteIdea() {
-    if (
-      !confirm(`Do you want to permanently delete '${this.state.ideaName}'?`)
-    ) {
+  const deleteIdea = async () => {
+    if (!confirm(`Do you want to permanently delete '${ideaName}'?`)) {
       return
     }
 
-    await db.deleteRecord(db.types.idea, this.state.id)
+    await db.deleteRecord(db.types.idea, id)
     const navigate = useNavigate()
     navigate('/')
   }
 
-  async handleAcceptUpdates() {
+  const handleAcceptUpdates = async () => {
     var updateObject = {
-      name: this.state.pendingName,
-      start_year: this.state.pendingStartYear,
-      end_year: this.state.pendingEndYear,
+      name: pendingName,
+      start_year: pendingStartYear,
+      end_year: pendingEndYear,
     }
 
-    db.updateRecord(db.types.idea, this.props.id, updateObject)
-    this.setState({ edit: false })
+    db.updateRecord(db.types.idea, id, updateObject)
+    setEdit(false)
   }
 
-  render() {
-    this.props.setPageTitle(this.state.pendingName)
-    return (
-      <div>
-        {/* Header and Edit */}
-        <div key="idea-information">
-          {this.state.edit ? (
-            <>
-              <label htmlFor="title" className="work-page form-label">
-                Name
-              </label>
-              <input
-                className="work-page title input"
-                id="title"
-                defaultValue={this.state.pendingName}
-                onChange={(e) => {
-                  this.setState({ pendingName: e.target.value })
-                }}
-              />
-              <label htmlFor="startYear" className="work-page form-label">
-                Start Year
-              </label>
-              <input
-                className="work-page title input"
-                id="startYear"
-                defaultValue={this.state.pendingStartYear}
-                onChange={(e) => {
-                  this.setState({ pendingStartYear: e.target.value })
-                }}
-              />
-              <label htmlFor="endYear" className="work-page form-label">
-                End Year
-              </label>
-              <input
-                className="work-page title input"
-                id="endYear"
-                defaultValue={this.state.pendingEndYear}
-                onChange={(e) => {
-                  this.setState({ pendingEndYear: e.target.value })
-                }}
-              />
-              <button
-                className="top-level standard-button left-right"
-                onClick={this.handleAcceptUpdates.bind(this)}
-              >
-                Done
-              </button>
-            </>
-          ) : (
-            <>
-              <div className="page-title">{this.state.pendingName}</div>
-              {this.state.pendingStartYear || this.state.pendingEndYear ? (
-                <div className="page-sub-title">
-                  {this.state.pendingStartYear ? (
-                    <YearSpan year={this.state.pendingStartYear} />
-                  ) : null}
-                  {this.state.pendingStartYear && this.state.pendingEndYear
-                    ? ' to '
-                    : null}
-                  {this.state.pendingEndYear ? (
-                    <YearSpan year={this.state.pendingEndYear} />
-                  ) : null}
-                </div>
-              ) : null}
-              <div>
-                <button
-                  className="top-level standard-button"
-                  onClick={this.deleteIdea.bind(this)}
-                >
-                  Delete
-                </button>
-                <button
-                  className="top-level standard-button"
-                  onClick={() => {
-                    this.setState({ edit: true })
-                  }}
-                >
-                  Edit
-                </button>
+  props.setPageTitle(pendingName)
+
+  return (
+    <div>
+      {/* Header and Edit */}
+      <div key="idea-information">
+        {edit ? (
+          <>
+            <label htmlFor="title" className="work-page form-label">
+              Name
+            </label>
+            <input
+              className="work-page title input"
+              id="title"
+              defaultValue={pendingName}
+              onChange={(e) => {
+                setPendingName(e.target.value)
+              }}
+            />
+            <label htmlFor="startYear" className="work-page form-label">
+              Start Year
+            </label>
+            <input
+              className="work-page title input"
+              id="startYear"
+              defaultValue={pendingStartYear}
+              onChange={(e) => {
+                setPendingStartYear(e.target.value)
+              }}
+            />
+            <label htmlFor="endYear" className="work-page form-label">
+              End Year
+            </label>
+            <input
+              className="work-page title input"
+              id="endYear"
+              defaultValue={pendingEndYear}
+              onChange={(e) => {
+                setPendingEndYear(e.target.value)
+              }}
+            />
+            <button
+              className="top-level standard-button left-right"
+              onClick={handleAcceptUpdates}
+            >
+              Done
+            </button>
+          </>
+        ) : (
+          <>
+            <div className="page-title">{pendingName}</div>
+            {pendingStartYear || pendingEndYear ? (
+              <div className="page-sub-title">
+                {pendingStartYear ? <YearSpan year={pendingStartYear} /> : null}
+                {pendingStartYear && pendingEndYear ? ' to ' : null}
+                {pendingEndYear ? <YearSpan year={pendingEndYear} /> : null}
               </div>
-            </>
-          )}
-        </div>
-
-        {/* Note List */}
-        <NoteList
-          key={'idea' + this.props.id}
-          viewMode={this.props.viewMode}
-          getListOfNotes={this.getListOfNotes.bind(this)}
-        />
+            ) : null}
+            <div>
+              <button
+                className="top-level standard-button"
+                onClick={deleteIdea}
+              >
+                Delete
+              </button>
+              <button
+                className="top-level standard-button"
+                onClick={() => {
+                  setEdit(true)
+                }}
+              >
+                Edit
+              </button>
+            </div>
+          </>
+        )}
       </div>
-    )
-  }
+
+      {/* Note List */}
+      <NoteList
+        key={'idea' + id}
+        viewMode={props.viewMode}
+        getListOfNotes={getListOfNotes}
+      />
+    </div>
+  )
 }
 
 export default Idea
