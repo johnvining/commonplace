05f90b6e3a04ab3c1316857d6c38c4f9be3838e1
John Vining <johnvining@gmail.com>
2023-12-27 12:10:14 -0600

    Update tags request to force JSON, prompt engineering, closes #291

--- server/src/utils/suggestions.js
+++ server/src/utils/suggestions.js
@@ -21,37 +21,38 @@ export const getSuggestedTitle = async function (note_text) {
   return chatCompletion.choices[0].message.content
 }
 
-// TODO: Look in to JSON return type
 export const getSuggestedIdeas = async function (note_title, note_text) {
   const chatCompletion = await openai.chat.completions.create({
     messages: [
       {
+        role: 'system',
+        content:
+          'You are a diligent assistant whose job is to categorize text and suggest a list of tags as JSON output.',
+      },
+      {
         role: 'user',
         content:
-          "Create a list of 10 tags for the following text, and add an additional list of 3 possible decades of the form (1950's) that the text is describing, " +
-          'including the comma before "s". Combine those lists and print them with the following formatting rules: 1) there should not be any quote ' +
-          'characters, 2) the tags should be separated by commas. For each tag, make every word lowercase except proper nouns. Do not print the original ' +
-          'lists, just print the combined list. Do not print a period at the end of your answer.\nThe Text: ' +
+          'Create a list of 10 possible tags that could be used to categorize the following text. They may be things like: people mentioned in the text, ' +
+          ' topics mentioned in the text, the type of text, or subjects the text is relevant to. Also, include up to three decades that are relevant to the ' +
+          " text in the format of 1950's. Format the results as a list of Sentence-case strings " +
+          ' except for proper nouns, which should be capitalized in JSON.' +
+          ' The Text: ' +
           note_title +
           '\n' +
           note_text,
       },
     ],
-    model: 'gpt-3.5-turbo',
+    model: 'gpt-4-1106-preview',
+    response_format: { type: 'json_object' },
   })
 
   let suggested_tags = chatCompletion.choices[0].message.content
-
-  // Server doesn't support .replaceAll apparently
-  return suggested_tags
-    .replace('\n', '')
-    .replace('Tags:', '')
-    .replace('.', '')
-    .split(',')
-    .map(function (tag) {
-      let trimmed = tag.trim()
-      return trimmed.charAt(0).toUpperCase() + trimmed.slice(1)
-    })
+  try {
+    let tags_json = JSON.parse(suggested_tags)
+    return tags_json.tags
+  } catch (e) {
+    return ['Not Valid JSON']
+  }
 }
 
 export const getLLMCorrectedText = async function (text) {
